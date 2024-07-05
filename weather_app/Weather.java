c53565b60b3cb3c1113fb3e145ba52fc4b477a75
Kummari Narendra <92687869+NarendraShalivahana@users.noreply.github.com>
2024-07-05 23:31:20 +0530

    Create Weather.java

---
+++ weather_app/Weather.java
@@ -0,0 +1,42 @@
+package com.example.weather_app;
+
+public class Weather {
+    private String city;
+    private String temperature;
+    private String description;
+    private String iconUrl;
+
+    // Getters and Setters
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+}
